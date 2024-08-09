85d22c88849a4d0ee706bbebc0267b32858416d5
LuBoys <lulu.58@outlook.fr>
2024-08-09 22:46:17 +0200

    delete animation

--- my-multi-builder-plugin.php
+++ my-multi-builder-plugin.php
@@ -18,8 +18,6 @@ function custom_widgets_enqueue_assets() {
     wp_enqueue_style('custom-widgets-style', plugin_dir_url(__FILE__) . 'css/style.css');
     wp_enqueue_script('gsap-js', 'https://cdnjs.cloudflare.com/ajax/libs/gsap/3.9.1/gsap.min.js', array(), null, true);
     wp_enqueue_script('custom-widgets-draggable-script', plugin_dir_url(__FILE__) . 'js/draggable.js', array('jquery', 'gsap-js'), null, true);
-    wp_enqueue_script('custom-widgets-animated-image-gallery-script', plugin_dir_url(__FILE__) . 'js/animated-image-gallery.js', array('jquery', 'gsap-js'), null, true);
-    wp_enqueue_script('custom-widgets-animated-text-script', plugin_dir_url(__FILE__) . 'js/animated-text.js', array('jquery', 'gsap-js'), null, true);
     wp_enqueue_script('custom-widgets-bouncing-text-script', plugin_dir_url(__FILE__) . 'js/bouncing-text.js', array('gsap-js'), null, true);
     wp_enqueue_script('custom-widgets-split-text-script', plugin_dir_url(__FILE__) . 'js/split-text.js', array('gsap-js'), null, true);
 
@@ -27,19 +25,6 @@ function custom_widgets_enqueue_assets() {
     wp_enqueue_style('custom-widgets-slide-fade-style', plugin_dir_url(__FILE__) . 'css/slide-fade.css');
     wp_enqueue_script('custom-widgets-slide-fade-script', plugin_dir_url(__FILE__) . 'js/slide-fade.js', array('gsap-js'), null, true);
 
-    // Enqueue scripts and styles for the Shrink Grow widget
-    wp_enqueue_style('custom-widgets-shrink-grow-style', plugin_dir_url(__FILE__) . 'css/shrink-grow.css');
-    wp_enqueue_script('custom-widgets-shrink-grow-script', plugin_dir_url(__FILE__) . 'js/shrink-grow.js', array('gsap-js'), null, true);
-
-    // Enqueue scripts and styles for the Glitch Text widget
-    wp_enqueue_style('custom-widgets-glitch-text-style', plugin_dir_url(__FILE__) . 'css/glitch-text.css');
-    wp_enqueue_script('custom-widgets-glitch-text-script', plugin_dir_url(__FILE__) . 'js/glitch-text.js', array('gsap-js'), null, true);
-
-    // Enqueue scripts and styles for the Wave Text widget
-    wp_enqueue_style('custom-widgets-wave-text-style', plugin_dir_url(__FILE__) . 'css/wave-style.css');
-    wp_enqueue_script('custom-widgets-wave-text-script', plugin_dir_url(__FILE__) . 'js/wave-text.js', array('gsap-js'), null, true);
-
-    // Enqueue scripts and styles for the Scroll Animation widget
     wp_enqueue_style('custom-widgets-scroll-animation-style', plugin_dir_url(__FILE__) . 'css/scroll-animation.css');
     wp_enqueue_script('scrolltrigger-js', 'https://cdnjs.cloudflare.com/ajax/libs/gsap/3.9.1/ScrollTrigger.min.js', array('gsap-js'), null, true);
     wp_enqueue_script('custom-widgets-scroll-animation-script', plugin_dir_url(__FILE__) . 'js/scroll-animation.js', array('gsap-js', 'scrolltrigger-js'), null, true);
@@ -59,12 +44,6 @@ function register_custom_elementor_widgets($widgets_manager) {
     require_once(__DIR__ . '/widgets/elementor/widget-draggable.php');
     $widgets_manager->register(new \Elementor\Widget_Draggable());
 
-    require_once(__DIR__ . '/widgets/elementor/widget-animated-image-gallery.php');
-    $widgets_manager->register(new \Elementor\Widget_Animated_Image_Gallery());
-
-    require_once(__DIR__ . '/widgets/elementor/widget-animated-text.php');
-    $widgets_manager->register(new \Elementor\Widget_Animated_Text());
-
     require_once(__DIR__ . '/widgets/elementor/widget-bouncing-text.php');
     $widgets_manager->register(new \Elementor\Widget_Bouncing_Text());
 
@@ -73,15 +52,6 @@ function register_custom_elementor_widgets($widgets_manager) {
 
     require_once(__DIR__ . '/widgets/elementor/widget-slide-fade.php');
     $widgets_manager->register(new \Elementor\Widget_Slide_Fade());
-
-    require_once(__DIR__ . '/widgets/elementor/widget-shrink-grow.php');
-    $widgets_manager->register(new \Elementor\Widget_Shrink_Grow());
-
-    require_once(__DIR__ . '/widgets/elementor/widget-glitch-text.php');
-    $widgets_manager->register(new \Elementor\Widget_Glitch_Text());
-
-    require_once(__DIR__ . '/widgets/elementor/widget-wave-text.php');
-    $widgets_manager->register(new \Elementor\Widget_Wave_Text());
 
     require_once(__DIR__ . '/widgets/elementor/widget-scroll-animation.php');
     $widgets_manager->register(new \Elementor\Widget_Scroll_Animation());
@@ -113,12 +83,6 @@ function custom_widgets_admin_page() {
         $draggable_enabled = isset($_POST['custom_widgets_draggable_enabled']) ? 'yes' : 'no';
         update_option('custom_widgets_draggable_enabled', $draggable_enabled);
 
-        $animated_image_gallery_enabled = isset($_POST['custom_widgets_animated_image_gallery_enabled']) ? 'yes' : 'no';
-        update_option('custom_widgets_animated_image_gallery_enabled', $animated_image_gallery_enabled);
-
-        $animated_text_enabled = isset($_POST['custom_widgets_animated_text_enabled']) ? 'yes' : 'no';
-        update_option('custom_widgets_animated_text_enabled', $animated_text_enabled);
-
         $bouncing_text_enabled = isset($_POST['custom_widgets_bouncing_text_enabled']) ? 'yes' : 'no';
         update_option('custom_widgets_bouncing_text_enabled', $bouncing_text_enabled);
 
@@ -127,15 +91,6 @@ function custom_widgets_admin_page() {
 
         $slide_fade_enabled = isset($_POST['custom_widgets_slide_fade_enabled']) ? 'yes' : 'no';
         update_option('custom_widgets_slide_fade_enabled', $slide_fade_enabled);
-
-        $shrink_grow_enabled = isset($_POST['custom_widgets_shrink_grow_enabled']) ? 'yes' : 'no';
-        update_option('custom_widgets_shrink_grow_enabled', $shrink_grow_enabled);
-
-        $glitch_text_enabled = isset($_POST['custom_widgets_glitch_text_enabled']) ? 'yes' : 'no';
-        update_option('custom_widgets_glitch_text_enabled', $glitch_text_enabled);
-
-        $wave_text_enabled = isset($_POST['custom_widgets_wave_text_enabled']) ? 'yes' : 'no';
-        update_option('custom_widgets_wave_text_enabled', $wave_text_enabled);
 
         $scroll_animation_enabled = isset($_POST['custom_widgets_scroll_animation_enabled']) ? 'yes' : 'no';
         update_option('custom_widgets_scroll_animation_enabled', $scroll_animation_enabled);
@@ -151,14 +106,9 @@ function custom_widgets_admin_page() {
     }
 
     $draggable_enabled = get_option('custom_widgets_draggable_enabled', 'no');
-    $animated_image_gallery_enabled = get_option('custom_widgets_animated_image_gallery_enabled', 'no');
-    $animated_text_enabled = get_option('custom_widgets_animated_text_enabled', 'no');
     $bouncing_text_enabled = get_option('custom_widgets_bouncing_text_enabled', 'no');
     $split_text_enabled = get_option('custom_widgets_split_text_enabled', 'no');
     $slide_fade_enabled = get_option('custom_widgets_slide_fade_enabled', 'no');
-    $shrink_grow_enabled = get_option('custom_widgets_shrink_grow_enabled', 'no');
-    $glitch_text_enabled = get_option('custom_widgets_glitch_text_enabled', 'no');
-    $wave_text_enabled = get_option('custom_widgets_wave_text_enabled', 'no');
     $scroll_animation_enabled = get_option('custom_widgets_scroll_animation_enabled', 'no');
     $parallax_enabled = get_option('custom_widgets_parallax_enabled', 'no');
     $reveal_blur_enabled = get_option('custom_widgets_reveal_blur_enabled', 'no');
@@ -175,24 +125,6 @@ function custom_widgets_admin_page() {
                         <label for="custom_widgets_draggable_enabled">
                             <input type="checkbox" name="custom_widgets_draggable_enabled" id="custom_widgets_draggable_enabled" value="yes" <?php checked($draggable_enabled, 'yes'); ?> />
                             Permet de déplacer des éléments sur la page.
-                        </label>
-                    </td>
-                </tr>
-                <tr valign="top">
-                    <th scope="row">Widget Galerie d'images animées (Animated Image Gallery)</th>
-                    <td>
-                        <label for="custom_widgets_animated_image_gallery_enabled">
-                            <input type="checkbox" name="custom_widgets_animated_image_gallery_enabled" id="custom_widgets_animated_image_gallery_enabled" value="yes" <?php checked($animated_image_gallery_enabled, 'yes'); ?> />
-                            Crée une galerie d'images animée avec divers effets.
-                        </label>
-                    </td>
-                </tr>
-                <tr valign="top">
-                    <th scope="row">Widget Texte animé (Animated Text)</th>
-                    <td>
-                        <label for="custom_widgets_animated_text_enabled">
-                            <input type="checkbox" name="custom_widgets_animated_text_enabled" id="custom_widgets_animated_text_enabled" value="yes" <?php checked($animated_text_enabled, 'yes'); ?> />
-                            Ajoute un texte animé à votre page avec divers effets.
                         </label>
                     </td>
                 </tr>
@@ -220,33 +152,6 @@ function custom_widgets_admin_page() {
                         <label for="custom_widgets_slide_fade_enabled">
                             <input type="checkbox" name="custom_widgets_slide_fade_enabled" id="custom_widgets_slide_fade_enabled" value="yes" <?php checked($slide_fade_enabled, 'yes'); ?> />
                             Anime le texte avec un effet de fondu glissé.
-                        </label>
-                    </td>
-                </tr>
-                <tr valign="top">
-                    <th scope="row">Widget Shrink Grow (Shrink Grow)</th>
-                    <td>
-                        <label for="custom_widgets_shrink_grow_enabled">
-                            <input type="checkbox" name="custom_widgets_shrink_grow_enabled" id="custom_widgets_shrink_grow_enabled" value="yes" <?php checked($shrink_grow_enabled, 'yes'); ?> />
-                            Anime le texte avec un effet de réduction et de croissance.
-                        </label>
-                    </td>
-                </tr>
-                <tr valign="top">
-                    <th scope="row">Widget Glitch Text (Glitch Text)</th>
-                    <td>
-                        <label for="custom_widgets_glitch_text_enabled">
-                            <input type="checkbox" name="custom_widgets_glitch_text_enabled" id="custom_widgets_glitch_text_enabled" value="yes" <?php checked($glitch_text_enabled, 'yes'); ?> />
-                            Anime le texte avec un effet de glitch moderne.
-                        </label>
-                    </td>
-                </tr>
-                <tr valign="top">
-                    <th scope="row">Widget Wave Text (Wave Text)</th>
-                    <td>
-                        <label for="custom_widgets_wave_text_enabled">
-                            <input type="checkbox" name="custom_widgets_wave_text_enabled" id="custom_widgets_wave_text_enabled" value="yes" <?php checked($wave_text_enabled, 'yes'); ?> />
-                            Anime le texte avec un effet de vague.
                         </label>
                     </td>
                 </tr>
